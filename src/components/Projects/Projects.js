f32eac8211d7db2f5822d011bbedb06e68b147d7
domingo <domalca@yahoo.es>
2024-12-23 13:08:46 +0100

    add tech stack logos to projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,6 +14,40 @@ import mvc from '../../images/mvc.png';
 import jquery from '../../images/jquery.png';
 import css from '../../images/css3.png';
 import bootstrap from '../../images/bootstrap.png';
+import apiRest from '../../images/apiRest.png';
+import oauth2 from '../../images/oauth2.png';
+import gTask from '../../images/gTask.png';
+import bingMap from '../../images/bingMap.png';
+import mysql from '../../images/mysql.png';
+import bladeOne from '../../images/bladeOne.png';
+import composer from '../../images/composer.png';
+import laravel from '../../images/laravel.png';
+import eloquent from '../../images/eloquent.png';
+import vue from '../../images/vue.png';
+import tailwind from '../../images/tailwind.png';
+import react from '../../images/reactjs.png';
+import parcel from '../../images/parcel.png';
+import eslint from '../../images/eslint.png';
+import husky from '../../images/husky.png';
+import prettier from '../../images/prettier.png';
+import bem from '../../images/bem.png';
+import reactNative from '../../images/reactNative.png';
+import figma from '../../images/figma.png';
+import expo from '../../images/expo.png';
+import android from '../../images/androidStudio.png';
+import prisma from '../../images/prisma.png';
+import sqlServer from '../../images/sqlServer.png';
+import azure from '../../images/azure.png';
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -28,31 +62,31 @@ const projects = [
   {
     title: 'Delivery app',
     image: delivery,
-    techIcons: [],
+    techIcons: [php, apiRest,oauth2,gTask,mysql,mvc,bingMap ],
     githubLink: 'https://github.com/tuusuario/delivery-app',
   },
   {
     title: 'Flashy app',
     image: flashy,
-    techIcons: [],
+    techIcons: [php,bladeOne,bootstrap,js,jquery,composer,mvc ],
     githubLink: 'https://github.com/tuusuario/menu-choose-app',
   },
   {
     title: 'Library manager app',
     image: library,
-    techIcons: [],
+    techIcons: [php,laravel,eloquent,vue,tailwind,apiRest],
     githubLink: 'https://github.com/tuusuario/library-manager-app',
   },
   {
     title: 'Whish list app',
     image: whishlist,
-    techIcons: [],
+    techIcons: [react,parcel,eslint,husky,prettier,bem],
     githIcons: 'https://github.com/tuusuario/whish-list-app',
   },
   {
     title: 'Meditation app',
     image: zenenti,
-    techIcons: [],
+    techIcons: [reactNative,figma,expo,android,prisma,sqlServer,azure],
     githubLink: 'https://github.com/tuusuario/meditation-app',
   }
 ];
