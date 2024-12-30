8906b651da30667acf409603cebbf4a7f76624af
domalca2 <alonsodm@yahoo.es>
2024-12-30 12:35:05 +0100

    add dark theme mode to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import styles from './footer.module.css';
 import linkindLogo from '../../images/linkedin.png';
 import githubLogo from '../../images/github.png';
 
 const Footer = () => {
+  const {theme} = useContext(ThemeContext);
+
   return (
     <footer className={styles['footer']}>
         <hr />
-      <div className={styles['footer-content']}>
-        <div className={styles["footer-left"]}>
+      <div className={styles["footer-content"]}>
+        <div className={`${styles["footer-left"]} ${theme === 'dark' ? 
+          styles.dark : ''
+        }`}>
+          { console.log('tema aplicado en footer-left:', theme)}
           <p>Domingo Alonso</p>
         </div>
         <div className={styles["footer-right"]}>
