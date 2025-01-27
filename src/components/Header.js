bddfcc9b20a719ccc20eacae33ce76f03e218655
domalca2 <alonsodm@yahoo.es>
2025-01-27 19:06:49 +0100

    add new funcionality to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import { FaLightbulb } from 'react-icons/fa';
 import React, {useContext, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import {ThemeContext} from '../contexts/ThemeContext';
 
 function Header() {
   const {toggleTheme} = useContext(ThemeContext);
-  console.log('toggleTheme:', toggleTheme);
   const [showModal, setShowModal] = useState(false);
+  const location = useLocation();
 
   const toggleModalHead = () => {
     setShowModal(!showModal);
   };
+
+  const isActive = (path) => location.pathname === path;
   return (
     <header className="d-flex justify-content-between align-items-center fixed-top">
       <div className="btn-container">
@@ -31,18 +33,54 @@ function Header() {
             <div className="modal-head">
             <button className="close-btn-head" onClick={toggleModalHead}>X</button>
               <ul className="modal-nav-head">
-                <li><Link to="/" onClick={toggleModalHead}>About</Link></li>
-                <li><Link to="/projects" onClick={toggleModalHead}>Projects</Link></li>
-                <li><Link to="/contact" onClick={toggleModalHead}>Contact</Link></li>
+                <li><Link 
+                      to="/" 
+                      onClick={toggleModalHead}
+                      className={isActive('/') ? 'active' : ''}
+                    >
+                    About
+                    </Link>
+                </li>
+                <li><Link 
+                      to="/projects" 
+                      onClick={toggleModalHead}
+                      className={isActive('/projects') ? 'active' : ''}
+                    >
+                    Projects
+                    </Link>
+                </li>
+                <li><Link 
+                      to="/contact" 
+                      onClick={toggleModalHead}
+                      className={isActive('/contact') ? 'active' : ''}
+                    >
+                    Contact
+                    </Link>
+                </li>
               </ul>
             </div>
           </div>
         )}
 
         <ul className="navbar-nav">
-          <li className="nav-item"><Link className="nav-link" to="/">About</Link></li>
-          <li className="nav-item"><Link className="nav-link" to="/projects">Projects</Link></li>
-          <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+          <li className="nav-item">
+            <Link className={`nav-link ${isActive('/') ? 'active' : ''}`} 
+            to="/">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className={`nav-link ${isActive('/projects') ? 'active' : ''}`} 
+            to="/projects">
+              Project
+            s</Link>
+          </li>
+          <li className="nav-item">
+            <Link className={`nav-link ${isActive('/contact') ? 'active' : ''}`}
+            to="/contact">
+              Contact
+            </Link>
+          </li>
         </ul>
       </nav>
       
